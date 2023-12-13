94f41b0008428a86d16b8e917711473256c76cfd
tristanratz <triratz@aol.com>
2023-12-13 17:32:06 +0100

    Add model

--- model.py
+++ model.py
@@ -1,12 +1,40 @@
-# Import necessary libraries
-from transformers import AutoModelForCausalLM, AutoTokenizer
+from dotenv import load_dotenv
+from transformers import AutoModelForCausalLM, AutoTokenizer, LlamaForCausalLM, LlamaTokenizer
+from langchain.chat_models import ChatOpenAI
+from langchain.llms.huggingface_pipeline import HuggingFacePipeline
+from datetime import date
+import transformers
 
-def load_model(path):
-    # Set up the model and tokenizer
-    model_name =  path
-    model = AutoModelForCausalLM.from_pretrained(model_name)
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
-    return model, tokenizer
+load_dotenv()
+
+class Model:
+
+    def __init__(self, model="openai", path = "/workspace/MasterThesis/models/llama7b-ch-hf/"):
+        self.model_type = model
+        if model=="openai":
+            self.llm = ChatOpenAI()
+            return
+        elif model == "llama":
+            tokenizer = LlamaTokenizer.from_pretrained(path, device_map='auto')
+            model_loaded = LlamaForCausalLM.from_pretrained(path, device_map='auto')
+        else:
+            model_loaded = AutoModelForCausalLM.from_pretrained(path)
+            tokenizer = AutoTokenizer.from_pretrained(path)
+        generate_text = transformers.pipeline(
+            model=model_loaded, 
+            tokenizer=tokenizer,
+            return_full_text=True,  # langchain expects the full text
+            task='text-generation',
+            repetition_penalty=1.1,
+            temperature=0.1,
+            do_sample=True,
+            max_length=500,
+        )
+        self.llm = HuggingFacePipeline(pipeline=generate_text)
+
+
+    def get_llm(self):
+        return self.llm
 
 def basic_inference(prompt, model, tokenizer):
     inputs = tokenizer(prompt, return_tensors="pt")
@@ -15,3 +43,26 @@ def basic_inference(prompt, model, tokenizer):
     answer = tokenizer.batch_decode(generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
 
     return answer
+
+if __name__ == "__main__":
+    m = Model("llama")
+    llm = m.get_llm()
+
+    
+    prompt = """<s><<SYS>>
+            You are a helpful, respectful and honest assistant called Rarag. Always do...
+
+            Date: {date}
+            If you are unsure about an answer, truthfully say "I don't know"
+            <</SYS>>
+
+            [INST] Remember you are an assistant [/INST] User:"""
+    prompt = str.format(prompt, date=date.today())
+    
+    inp = "Introduce yourself!"
+
+    while inp != "exit":
+        for chunk in llm.stream(prompt + inp):
+            print(chunk, end="", flush=True)
+        print("\n\n")
+        inp = input("> ")
