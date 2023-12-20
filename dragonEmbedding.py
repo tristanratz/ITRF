4cefe3355439b9adc8d3eff3c5b2966f56acc7c4
tristanratz <triratz@aol.com>
2023-12-20 16:06:29 +0000

    move dragon embedding

--- dragonEmbedding.py
+++
@@ -1,5 +0,0 @@
-# Base on HuggingFace Adapter
-# https://api.python.langchain.com/en/stable/_modules/langchain/embeddings/huggingface.html#HuggingFaceEmbeddings
-
-# Test if embed documents is called in case of bulk call
-# This would pose a problem with our approach (DRAGON is asymetric)
