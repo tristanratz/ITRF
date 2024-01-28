0b1586350a018ad67dd16ba5158031cf9469fc73
tristanratz <triratz@aol.com>
2024-01-28 23:23:23 +0000

    Add cc loader

--- create_corpus/load_cc.py
+++ create_corpus/load_cc.py
@@ -12,18 +12,19 @@ import argparse
 # Parse arguments
 parser = argparse.ArgumentParser()
 parser.add_argument('--process', type=int, default=0, help='The number of the process')
-parser.add_argument('--console', type=bool, default=False, help='Output the results to the console')
+parser.add_argument('--tofile', type=bool, default=False, help='Output the results to the console')
 args = parser.parse_args()
 
 process = args.process
 
-if not args.console:
+if args.tofile:
     # Put the output in a file
-    f = open(f"../data/process_output/wiki_dump-{process}.txt", "w")
+    f = open(f"../data/process_output/cc_dump-{process}.txt", "w")
     sys.stdout = f
 
 # Load wikipedia corpus (in steps of 1000 documents)
 documents_count = 0
+total_count = 0
 chunks_count = 0
 start = time.time()
 
@@ -33,24 +34,16 @@ def len_func(example):
 # Create retriever
 text_splitter = RecursiveCharacterTextSplitter(
     # Set a really small chunk size, just to show.
-    chunk_size=200,
+    chunk_size=100,
     chunk_overlap=0,
     length_function=len_func,
     is_separator_regex=False,
 )
 device = "cuda:" + str(process)
-# tokenizer = LlamaTokenizer.from_pretrained("../models/llama7b", device_map=device) # Tokenizer not used for text splitting
 embedding = HuggingFaceBgeEmbeddings(model_name="../models/retriever/bge-base-en-v1.5", model_kwargs={ "device": device })
 
 # Create the retriever
 client = QdrantClient(url="http://localhost:6333")
-
-# Optional: Delete the collection if it already exists
-# client.delete_collection(collection_name="retriever")
-# client.create_collection(collection_name="retriever", vectors_config=models.VectorParams(
-#         size=768,  # Vector size is defined by used model
-#         distance=models.Distance.COSINE,
-#     ))
 
 db = Qdrant(client, 
             collection_name="retriever",
@@ -59,61 +52,66 @@ db = Qdrant(client,
 
 
 
-def index_doc(documents, metadatas):
+def index_doc(docs, nr_documents):
     global documents_count
+    global total_count
     global chunks_count
     global start
     global db
     global text_splitter
-    global tokenizer
     global embedding
     global client
 
     process_start = time.time()
-    docs = text_splitter.create_documents(documents, metadatas)
     db.add_documents(docs)
     elapsed = (time.time() - start)
     clock = str(timedelta(seconds=elapsed))
-    chunks_count += len(docs)
-    print(f"Loaded documents {len(documents)} (total: {documents_count}), with {len(docs)} chunks (total: {chunks_count}), {clock} elapsed (process: {time.time()-process_start}s)")
+    print(f"Loaded documents {nr_documents} (total: {documents_count} [frac: {round(documents_count%total_count, 2)}%]), with {len(docs)} chunks (total: {chunks_count}), {clock} elapsed (embedding: {time.time()-process_start}s)")
 
 def main(process=0):
     global documents_count
+    global total_count
     global chunks_count
     global start
     global db
     global text_splitter
-    global tokenizer
     global embedding
     global client
 
-    with open("../data/corpora/wiki/enwiki-dec2021/text-list-100-sec.jsonl", mode="r") as f:
-        documents = []
-        metadatas = []
-        tasks = []
-        print("Loading documents...")
-        for line in f:
-            
-            json_line = loads(line)
 
-            # Split document into chunks  
-            if documents_count % 4 == process:
-                documents.append(json_line["text"])
-                metadatas.append({"title": json_line["title"], "section": json_line["section"]})  
+    dataset = load_dataset('oscar', "unshuffled_deduplicated_en", split='train', streaming=True)
+    shuffled_dataset = iter(dataset.shuffle(buffer_size=10_000, seed=2024))
+
+    documents = [] # Buffer of split documents
+    chunk_nr = 0
+    doc_nr = 0
+
+    print("Loading documents...")
+    while chunks_count < (110_000_000 / 4):            
+        sample = next(shuffled_dataset)
+        
+        # Split document into chunks  
+        # Use only 1/4 of the documents for each process
+        if total_count % 4 == process:
+            docs = text_splitter.create_documents([sample["text"]], [{"id": sample["id"]}])
+            documents.extend(docs)
+            chunk_nr += len(docs)
+            doc_nr += 1
             documents_count += 1
 
-            if documents_count % 4_000 == 0:
-                index_doc(documents, metadatas)
+        total_count += 1
 
-                # results = await db.aadd_documents(new_docs)
-                # print(results)
-                documents = []
-                metadatas = []
+        if chunk_nr > 1_000:
+            index_doc(documents, doc_nr)
+
+            chunks_count += chunk_nr
+            # results = await db.aadd_documents(new_docs)
+            # print(results)
+            docs = []
+            chunk_nr = 0
+            doc_nr = 0
                 
-
-        new_docs = text_splitter.create_documents(documents, metadatas)            
-        chunks_count += len(new_docs)
-        db.add_documents(new_docs)
+    db.add_documents(documents)
         
 
     print("---------------------------------------------")
@@ -126,3 +124,4 @@ def main(process=0):
 
 if __name__ == "__main__":
     main(process=process)
+    
