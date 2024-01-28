d261ee5e2e6639b494b88d4bef0a29daf8803456
tristanratz <triratz@aol.com>
2024-01-28 23:34:21 +0000

    Add worker scripts

---
+++ create_corpus/run_cc.sh
@@ -0,0 +1,11 @@
+nohup python3 -u load_cc.py --process 0 >> ../data/process_output/cc_0.log 2>&1 &
+echo $! > ../data/process_output/save_pid_0.txt
+
+nohup python3 -u load_cc.py --process 1 >> ../data/process_output/cc_1.log 2>&1 &
+echo $! > ../data/process_output/save_pid_1.txt
+
+nohup python3 -u load_cc.py --process 2 >> ../data/process_output/cc_2.log 2>&1 &
+echo $! > ../data/process_output/save_pid_2.txt
+
+nohup python3 -u load_cc.py --process 3 >> ../data/process_output/cc_3.log 2>&1 &
+echo $! > ../data/process_output/save_pid_3.txt
