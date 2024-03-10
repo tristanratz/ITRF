9b8ff66a8220a06b657593c5c8bbb27a38afee42
tristanratz <triratz@aol.com>
2024-03-10 13:39:42 +0000

    Clean directory train_llm

--- train_llm/run_training.sh
+++ train_llm/run_training.sh
@@ -1,2 +1,2 @@
-nohup accelerate launch --config_file ./accelerate_config3.yaml ./train_llm.py  >> ../data/process_output/training_llm_qlora.log 2>&1 &
+nohup accelerate launch --config_file ./accelerate_config.yaml ./train_llm.py  >> ../data/process_output/training_llm_qlora.log 2>&1 &
 echo $! > ../data/process_output/save_pid_llm_training.txt
